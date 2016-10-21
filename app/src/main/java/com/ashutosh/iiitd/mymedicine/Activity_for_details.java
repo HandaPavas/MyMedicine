5d866a1fd928d6d516fbaa818fb59af2057bee91
ashutosh16013 <ashutosh16013@iiitd.ac.in>
2016-10-21 19:39:18 +0530

    Commit 4

--- app/src/main/java/com/ashutosh/iiitd/mymedicine/Activity_for_details.java
+++ app/src/main/java/com/ashutosh/iiitd/mymedicine/Activity_for_details.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -13,7 +14,10 @@ public class Activity_for_details extends AppCompatActivity {
 
     protected Button mButton_for_redirect;
     private final static String KEY_NUMBERS = "number_of_medicine";
+    private final static String KEY_TAB_ID = "medicine_table_id";
     TextView numbers;
+    TextView doc_name;
+    TextView hosp_name;
     int num = 0;
 
     @Override
@@ -25,19 +29,28 @@ public class Activity_for_details extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 numbers = (TextView)findViewById(R.id.input_number);
-                try
-                {
-                    num = Integer.parseInt(numbers.getText().toString());
-                }
-                catch(Exception ex)
-                {
+                doc_name = (TextView)findViewById(R.id.input_doc_name);
+                hosp_name = (TextView)findViewById(R.id.input_hospital);
+                if(!(doc_name.getText().toString().equals("") || hosp_name.getText().toString().equals("") ||
+                        numbers.getText().toString().equals(""))) {
+                    try {
+                        num = Integer.parseInt(numbers.getText().toString());
+                    } catch (Exception ex) {
 
+                    }
+                    DBHelper db = new DBHelper(getApplicationContext());
+                    int id = db.insert_data_into_doctor_details(doc_name.getText().toString(), hosp_name.getText().toString());
+                    Intent intent_for_adding = new Intent(Activity_for_details.this, Add_medicines_2.class);
+                    intent_for_adding.putExtra(KEY_NUMBERS, num);
+                    intent_for_adding.putExtra(KEY_TAB_ID, id);
+                    //Toast.makeText(getApplicationContext(), "Inserted " + id, Toast.LENGTH_SHORT).show();
+                    startActivity(intent_for_adding);
+                    overridePendingTransition(R.anim.right_slide_in, R.anim.right_slide_out);
+                    finish();
                 }
-                Intent intent_for_adding = new Intent(Activity_for_details.this, Add_medicines_2.class);
-                intent_for_adding.putExtra(KEY_NUMBERS,num);
-                startActivity(intent_for_adding);
-                overridePendingTransition  (R.anim.right_slide_in, R.anim.right_slide_out);
-                finish();
+                else{
+                    Toast.makeText(getApplicationContext(), "Enter data in all the fields !", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
