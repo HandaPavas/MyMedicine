8ae4b3e62a3e8aeb3f12c27a802f4a065041beb2
ashutosh16013 <ashutosh16013@iiitd.ac.in>
2016-11-28 23:44:46 +0530

    commit at 11:44

--- app/src/main/java/com/ashutosh/iiitd/mymedicine/alarm_activity.java
+++ app/src/main/java/com/ashutosh/iiitd/mymedicine/alarm_activity.java
@@ -12,47 +12,84 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.provider.AlarmClock;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class alarm_activity extends AppCompatActivity
-        implements AdapterView.OnItemSelectedListener
-{
-    Spinner spinner2,spinner3;
+public class alarm_activity extends AppCompatActivity{
+
     private Alarm_adapter mAdapterTime;
     private ArrayList<Alarm_row> alarmList = new ArrayList<>();
     RecyclerView rv_edit_alarm;
-    private int hour_alarm_1, minute_alarm_1, hour_alarm_2, minute_alarm_2, hour_alarm_3, minute_alarm_3;
-    private String tag_for_alarm = "";
-
-    String categories[]={"Daily","Weekly","Monthly"};
-    String Duration[]={"1","2","3","4","5","6"};
+    boolean medicine_display_state = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle from_details = getIntent().getExtras();
-        //int alarm_type_id = from_details.getInt("alarm_table_id");
-        //tag_for_alarm = from_details.getString("med_name");
         setContentView(R.layout.activity_alarm_activity);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
-        spinner3 = (Spinner) findViewById(R.id.spinner3);
-        // spinner3.setVisibility(View.GONE);
-        //Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
-
-        // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
-        spinner2.setOnItemSelectedListener(this);
-
-        // Spinner Drop down elements
 
         //Recycler view for editing alarms
+        ArrayList<Medicine> selected_medicines = (ArrayList<Medicine>)from_details.getSerializable("KEY_FOR_LIST");
+        //Setting medicines text view
+        TextView tv_all_medicines = (TextView)findViewById(R.id.tv_all_medicines);
+        Iterator<Medicine> it = selected_medicines.iterator();
+        String all_medicines = "\n";
+        while(it.hasNext()){
+            all_medicines += it.next().getMed_name() + "\n";
+        }
+        tv_all_medicines.setText(all_medicines);
+
+        //setting image button for hiding
+        final ImageButton btn_view_all = (ImageButton)findViewById(R.id.btn_view_all);
+        btn_view_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(medicine_display_state == false){
+                    medicine_display_state = true;
+                    btn_view_all.setImageResource(R.drawable.ic_remove_black_24px);
+                    LinearLayout ll = (LinearLayout)findViewById(R.id.ll_medicines);
+                    ll.setVisibility(View.VISIBLE);
+                }
+                else{
+                    medicine_display_state = false;
+                    btn_view_all.setImageResource(R.drawable.ic_add_black_24px);
+                    LinearLayout ll = (LinearLayout)findViewById(R.id.ll_medicines);
+                    ll.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        //setting spinner for frequency
+        Spinner sp_alarm_frequency = (Spinner) findViewById(R.id.sp_alarm_frequency);
+        List<String> frequency = new ArrayList<String>();
+        frequency.add("Daily");
+        frequency.add("Monthly");
+        frequency.add("Weekly");
+        ArrayAdapter<String> frequency_adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, frequency);
+        frequency_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_alarm_frequency.setAdapter(frequency_adapter);
+
+        //setting spinner for days
+        Spinner sp_days = (Spinner)findViewById(R.id.sp_days);
+        List<Integer> days = new ArrayList<Integer>();
+        for(int i = 1 ; i <= 31 ; i++){
+            days.add(i);
+        }
+        ArrayAdapter<Integer> days_adapter = new ArrayAdapter<Integer>(
+                this, android.R.layout.simple_spinner_item, days);
+        sp_days.setAdapter(days_adapter);
+
 
         rv_edit_alarm = (RecyclerView)findViewById(R.id.rv_edit_alarm);
         mAdapterTime = new Alarm_adapter(alarmList, getApplicationContext());
@@ -63,24 +100,6 @@ public class alarm_activity extends AppCompatActivity
         rv_edit_alarm.setAdapter(mAdapterTime);
         prepareInitialAlarmData();
 
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
-        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Duration);
-        dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(dataAdapter2);
-        List<String> weekdays = new ArrayList<String>();
-        weekdays.add("Sunday");
-        weekdays.add("Monday");
-        weekdays .add("Tuesday");
-        weekdays.add("Wednesday");
-        weekdays.add("Thursday");
-        weekdays.add("Friday");
-        weekdays.add("Saturday");
-        ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, weekdays);
-        dataAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner3.setAdapter(dataAdapter3);
     }
 
     //prepare Data for alarm recycler view
@@ -100,141 +119,5 @@ public class alarm_activity extends AppCompatActivity
     public void onBackPressed() {
         setResult(Activity.RESULT_CANCELED);
         super.onBackPressed();
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestcode, int resultcode, Intent data )
-    {
-        if(requestcode==1)
-            if(resultcode==RESULT_OK)
-            {
-                int hr=data.getIntExtra("Hour",9);
-                hour_alarm_1 = hr;
-                int min=data.getIntExtra("Minute",30);
-                minute_alarm_1 = min;
-                String str="AM";
-                if(hr>=12)
-                    str="PM";
-                if(hr>12)
-                    hr=hr-12;
-
-                String hr1=String.valueOf(hr);
-                if(hr<10)
-                    hr1="0"+hr1;
-                String min1=String.valueOf(min);
-                if(min<10)
-                    min1="0"+min1;
-
-
-            }
-        if(requestcode==2)
-            if(resultcode==RESULT_OK)
-            {
-                int hr=data.getIntExtra("Hour",9);
-                hour_alarm_2 = hr;
-                int min=data.getIntExtra("Minute",30);
-                minute_alarm_2 = min;
-                String str="AM";
-                if(hr>=12)
-                    str="PM";
-                if(hr>12)
-                    hr=hr-12;
-
-                String hr1=String.valueOf(hr);
-                if(hr<10)
-                    hr1="0"+hr1;
-                String min1=String.valueOf(min);
-                if(min<10)
-                    min1="0"+min1;
-            }
-
-        if(requestcode==3)
-            if(resultcode==RESULT_OK)
-            {
-                int hr=data.getIntExtra("Hour",9);
-                hour_alarm_3 = hr;
-                int min=data.getIntExtra("Minute",30);
-                minute_alarm_3 = min;
-                String str="AM";
-                if(hr>=12)
-                    str="PM";
-                if(hr>12)
-                    hr=hr-12;
-
-                String hr1=String.valueOf(hr);
-                if(hr<10)
-                    hr1="0"+hr1;
-                String min1=String.valueOf(min);
-                if(min<10)
-                    min1="0"+min1;
-            }
-    }
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        Spinner spin = (Spinner)parent;
-        Spinner spin2 = (Spinner)parent;
-        if(spin.getId() == R.id.spinner)
-        {
-            //  Toast.makeText(this, "Your choose :" + categories[ position],Toast.LENGTH_SHORT).show();
-            String item = spin.getItemAtPosition(position).toString();
-            TextView textView=(TextView)findViewById(R.id.text);
-            if(item.equals("Daily"))
-                textView.setText("Days");
-            if(item.equals("Weekly")) {
-
-                textView.setText("Weeks");
-
-                spinner3.performClick();
-            }
-            if(item.equals("Monthly"))
-                textView.setText("Months");
-        }
-        if(spin2.getId() == R.id.spinner2)
-        {
-            //  Toast.makeText(this, "Your choose :" + Duration[position],Toast.LENGTH_SHORT).show();
-        }
-
-
-
-
-        // Showing selected spinner item
-        //     Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
-
-    }
-
-    public void SetAlarm(View v){
-        //Toast.makeText(getApplicationContext(), "Fuck off !", Toast.LENGTH_SHORT).show();
-        //Spinner repeat = (Spinner)findViewById(R.id.spinner1);
-        //Spinner duration = (Spinner)findViewById(R.id.spinner2);
-        //String repeat_time = repeat.getSelectedItem().toString();
-        //int time = Integer.parseInt(duration.getSelectedItem().toString());
-        int hour_increment = 0;
-        int min_increment = 0;
-        boolean alarm_set = false;
-        /*if(repeat_time.equals("Daily")){
-
-
-        }
-        else if(repeat_time.equals("Weekly")){
-            //do increment for Weekly
-        }
-        else{
-            //do increment for Monthly
-        }*/
-        //currently doing only for 3 alarms. Will be done dynamically later
-
-        Toast.makeText(getApplicationContext(), "Alarm Set !", Toast.LENGTH_SHORT).show();
-        if(alarm_set == true){
-            setResult(RESULT_OK);
-            finish();
-        }
-        else{
-            setResult(RESULT_CANCELED);
-            finish();
-        }
     }
 }
